1f18ab47a6be40ba6211df2cd8b85d9378ec8abf
masataka <ar.tec.masa@gmail.com>
2018-06-01 07:35:59 +0900

    ジェネレータ関数を作成

--- arMaker.py
+++ arMaker.py
@@ -4,4 +4,16 @@
 import cv2
 
 aruco = cv2.aruco
+# dictionaryを生成
 dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+
+def arGenerator():
+	fileName = "images/ar.png"
+	generator = aruco.drawMarker(dictionary, 0, 100)
+	cv2.imwrite(fileName, generator)
+
+    img = cv2.imread(fileName)
+    cv2.imshow('ArMaker', img)
+    cv2.waitKey(0)
+
+arGenerator()
